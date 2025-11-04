74dd85c9f2c562723886f0f428db11eb2342939d
andres <aepirela@miuandes.cl>
2025-11-03 21:38:25 -0300

    nivel 3 listo

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -5,7 +5,7 @@ public class MainMenuController : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene("Game");
+        SceneManager.LoadScene("Level3");
     }
 
     public void QuitGame()
